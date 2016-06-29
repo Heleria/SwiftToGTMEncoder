afead1daf196b1d273778a5f8805210b8b68761e
Heleria <Jacob.R.Developer@gmail.com>
2016-06-30 01:49:04 +0200

    Initial Commit

---
+++ SwiftToGTMEncoder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftToGTMEncoder
+//
+//  Created by Jacob R on 30/06/2016.
+//  Copyright © 2016 Heleria. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
